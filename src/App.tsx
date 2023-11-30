eeca571829a3ceda0ed08a16a9283fc8298ee93f
Kenneth Duverge <kenneth.duverge@icloud.com>
2023-11-30 07:20:17 -0500

    Back with vimeo ux components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { Button } from '@chakra-ui/react';
+// import { Button } from '@chakra-ui/react';
+import { Button } from '@vimeo-ux/components';
 import './App.css';
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-// import { ThemeProvider } from '@vimeo-ux/components';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ThemeProvider as ChakraProvider } from '@vimeo-ux/components';
+// import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
