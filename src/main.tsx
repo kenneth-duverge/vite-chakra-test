527b04ed164897aca9750d7304b8ea503e0c3791
Kenneth Duverge <kenneth.duverge@icloud.com>
2023-11-30 07:13:58 -0500

    With bokeh theme and button

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { ThemeProvider } from '@vimeo-ux/components';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ThemeProvider>
       <App />
-    </ChakraProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
