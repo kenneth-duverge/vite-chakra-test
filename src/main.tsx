2823f13570446da311b4d99139ee26cfb0f214c4
Kenneth Duverge <kenneth.duverge@icloud.com>
2023-11-30 07:18:42 -0500

    With chakra provider and button

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { ThemeProvider } from '@vimeo-ux/components';
+// import { ThemeProvider } from '@vimeo-ux/components';
+import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
+    <ChakraProvider>
       <App />
-    </ThemeProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
